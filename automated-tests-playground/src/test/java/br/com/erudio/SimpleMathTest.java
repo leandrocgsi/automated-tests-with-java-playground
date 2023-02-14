ca404e6cbc4008b7dd1b96e247eead02be4bc65a
leandrocgsi <leandrocgsi@gmail.com>
2023-02-14 11:53:58 -0300

    First steps with JUnit 5 - Assertion Messages

--- automated-tests-playground/src/test/java/br/com/erudio/SimpleMathTest.java
+++ automated-tests-playground/src/test/java/br/com/erudio/SimpleMathTest.java
@@ -8,86 +8,86 @@ import br.com.erudio.math.SimpleMath;
 
 class SimpleMathTest {
 	
-@Test
-void testSum() {
+	@Test
+	void testSum() {
+		
+		SimpleMath math = new SimpleMath();
+		
+		double firstNumber = 6.2D;
+		double secondNumber = 2D;
+		double expectedResult = 8.2D;
+		
+		Double result = math.sum(firstNumber, secondNumber);
+		assertEquals(expectedResult, result,
+			() -> firstNumber + "+" + secondNumber + " did not produce " + expectedResult + "!");
+	}
 	
-	SimpleMath math = new SimpleMath();
+	@Test
+	void testSubtraction() {
+		
+		SimpleMath math = new SimpleMath();
+		
+	    double firstNumber = 6.2D;
+		double secondNumber = 2D;
+		double expectedResult = 4.2D;
+		
+		Double result = math.subtraction(firstNumber, secondNumber);
+		assertEquals(expectedResult, result,
+			() -> firstNumber + "-" + secondNumber +" did not produce " + expectedResult+ "!");
+	}
 	
-	double firstNumber = 6.2D;
-	double secondNumber = 2D;
-	double expectedResult = 8.2D;
+	@Test
+	void testMultiplication() {
+		
+		SimpleMath math = new SimpleMath();
+		
+	    double firstNumber = 6.2D;
+		double secondNumber = 2D;
+		double expectedResult = 12.4D;
+		
+		Double result = math.multiplication(firstNumber, secondNumber);
+		assertEquals(expectedResult, result,
+			() -> firstNumber + "*" + secondNumber + " did not produce " + expectedResult + "!");
+	}
 	
-	Double result = math.sum(firstNumber, secondNumber);
-	assertEquals(expectedResult, result,
-		() -> firstNumber + "+" + secondNumber + " did not produce " + expectedResult + "!");
-}
-
-@Test
-void testSubtraction() {
+	@Test
+	void testDivision() {
+		
+		SimpleMath math = new SimpleMath();
+		
+	    double firstNumber = 6.2D;
+		double secondNumber = 2D;
+		double expectedResult = 3.1D;
+		
+	    Double result = math.division(firstNumber, secondNumber);
+		assertEquals(expectedResult, result,
+			() -> firstNumber + "/" + secondNumber + " did not produce " + expectedResult + "!");
+	}
 	
-	SimpleMath math = new SimpleMath();
+	@Test
+	void testMeam() {
+		
+		SimpleMath math = new SimpleMath();
+		
+	    double firstNumber = 6.2D;
+		double secondNumber = 2D;
+		double expectedResult = 4.1D;
+		
+		Double result = math.mean(firstNumber, secondNumber);
+		assertEquals(expectedResult, result,
+			() -> "(" + firstNumber + "+" + secondNumber + ")/" + secondNumber + " did not produce " + expectedResult + "!");
+	}
 	
-    double firstNumber = 6.2D;
-	double secondNumber = 2D;
-	double expectedResult = 4.2D;
-	
-	Double result = math.subtraction(firstNumber, secondNumber);
-	assertEquals(expectedResult, result,
-		() -> firstNumber + "-" + secondNumber +" did not produce " + expectedResult+ "!");
-}
-
-@Test
-void testMultiplication() {
-	
-	SimpleMath math = new SimpleMath();
-	
-    double firstNumber = 6.2D;
-	double secondNumber = 2D;
-	double expectedResult = 12.4D;
-	
-	Double result = math.multiplication(firstNumber, secondNumber);
-	assertEquals(expectedResult, result,
-		() -> firstNumber + "*" + secondNumber + " did not produce " + expectedResult + "!");
-}
-
-@Test
-void testDivision() {
-	
-	SimpleMath math = new SimpleMath();
-	
-    double firstNumber = 6.2D;
-	double secondNumber = 2D;
-	double expectedResult = 3.1D;
-	
-    Double result = math.division(firstNumber, secondNumber);
-	assertEquals(expectedResult, result,
-		() -> firstNumber + "/" + secondNumber + " did not produce " + expectedResult + "!");
-}
-
-@Test
-void testMeam() {
-	
-	SimpleMath math = new SimpleMath();
-	
-    double firstNumber = 6.2D;
-	double secondNumber = 2D;
-	double expectedResult = 4.1D;
-	
-	Double result = math.mean(firstNumber, secondNumber);
-	assertEquals(expectedResult, result,
-		() -> "(" + firstNumber + "+" + secondNumber + ")/" + secondNumber + " did not produce " + expectedResult + "!");
-}
-
-@Test
-void testSquareRoot() {
-	
-	SimpleMath math = new SimpleMath();
-	
-	double number = 81D;
-	double expected = 9D;
-	
-	Double result = math.squareRoot(number);
-	assertEquals(expected, result,
-		() -> "Square Root of "+ number + " did not produce "+ expected + "!");
-}
+	@Test
+	void testSquareRoot() {
+		
+		SimpleMath math = new SimpleMath();
+		
+		double number = 81D;
+		double expected = 9D;
+		
+		Double result = math.squareRoot(number);
+		assertEquals(expected, result,
+			() -> "Square Root of "+ number + " did not produce "+ expected + "!");
+	}
 }
