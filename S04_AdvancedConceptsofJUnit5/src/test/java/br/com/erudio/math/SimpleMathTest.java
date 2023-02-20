6f921f9f2fd8a4340d25922508aa8ff9de197a9f
leandrocgsi <leandrocgsi@gmail.com>
2023-02-20 17:42:42 -0300

    Advanced Concepts of JUnit 5

--- S04_AdvancedConceptsofJUnit5/src/test/java/br/com/erudio/math/SimpleMathTest.java
+++ S04_AdvancedConceptsofJUnit5/src/test/java/br/com/erudio/math/SimpleMathTest.java
@@ -2,9 +2,13 @@ package br.com.erudio.math;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 @DisplayName("Test Math Operations in SimpleMath Class")
 class SimpleMathTest {
@@ -14,21 +18,32 @@ class SimpleMathTest {
     @BeforeEach
     void beforeEachMethod() {
         math = new SimpleMath();
-        System.out.println("Running @BeforeEach method");
     }
 
-    @DisplayName("Test 6.2 / 2 = 3.1")
-    @Test
-    void testDivision() {
-        System.out.println("Test 6.2 / 2 = 3.1");
+    @DisplayName("Test double subtraction [firstNumber, secondNumber, expectedResult]")
+    //@Test
+    @ParameterizedTest
+    //@MethodSource("testDivisionInputParameters")
+    @MethodSource()
+    void testDivision(
+            double firstNumber,
+            double secondNumber,
+            double expectedResult) {
         
-        double firstNumber = 6.2D;
-        double secondNumber = 2D;
-        double expectedResult = 3.1D;
+        System.out.println("Test " + firstNumber +
+                " / " + secondNumber + " = " + expectedResult + "");
         
         Double result = math.division(firstNumber, secondNumber);
-        assertEquals(expectedResult, result,
+        assertEquals(expectedResult, result, 100D,
             () -> firstNumber + "/" + secondNumber + " did not produce " + expectedResult + "!");
     }
 
+    public static Stream<Arguments> testDivision() {
+    //public static Stream<Arguments> testDivisionInputParameters() {
+        return Stream.of(
+            Arguments.of(6.2D, 2D, 3.1D),
+            Arguments.of(71D, 14D, 57D),
+            Arguments.of(18.3, 3.1D, 14,9D)
+        );
+    }
 }
