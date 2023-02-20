f9b8bb23bfb9b106e4cf6e36170470b0e25f5e87
leandrocgsi <leandrocgsi@gmail.com>
2023-02-20 16:43:57 -0300

    First Steps in JUnit 5 with Java 19

---
+++ S03_HelloJUnit5/src/test/java/br/com/erudio/ArraysCompareTest.java
@@ -0,0 +1,35 @@
+package br.com.erudio;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+//@Timeout(value = 100, unit = TimeUnit.MILLISECONDS)
+class ArraysCompareTest {
+
+    @Test
+    void testArraysSort() {
+        int[] numbers = {12,3,4,1};
+        int[] expectedArray = {1,3,4,12};
+        
+        Arrays.sort(numbers);
+        assertArrayEquals(numbers, expectedArray);
+    }
+
+    @Test
+    //@Timeout(1)
+    @Timeout(value = 100, unit = TimeUnit.MILLISECONDS)
+    void testSortPerformance() {
+        int array[] = {9,16,7,22};
+        for (int i = 0; i < 100000000; i++) {
+            array[0] = i;
+            Arrays.sort(array);
+        }
+    }
+
+}
