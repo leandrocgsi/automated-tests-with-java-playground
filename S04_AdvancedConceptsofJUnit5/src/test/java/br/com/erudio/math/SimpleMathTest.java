25674c0e6f9df7e5c27214e26833ac7261c6c8d5
leandrocgsi <leandrocgsi@gmail.com>
2023-02-20 17:08:54 -0300

    Advanced Concepts of JUnit 5

--- S04_AdvancedConceptsofJUnit5/src/test/java/br/com/erudio/math/SimpleMathTest.java
+++ S04_AdvancedConceptsofJUnit5/src/test/java/br/com/erudio/math/SimpleMathTest.java
@@ -1,11 +1,7 @@
 package br.com.erudio.math;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
