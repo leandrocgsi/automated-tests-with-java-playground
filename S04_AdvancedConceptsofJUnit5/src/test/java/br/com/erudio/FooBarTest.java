b61891d8f8da4c0f20c2a8d6cef3deecbf4a1186
leandrocgsi <leandrocgsi@gmail.com>
2023-02-20 16:53:21 -0300

    Advanced Concepts of JUnit 5

---
+++ S04_AdvancedConceptsofJUnit5/src/test/java/br/com/erudio/FooBarTest.java
@@ -0,0 +1,13 @@
+package br.com.erudio;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class FooBarTest {
+    @Test
+    void test() {
+        //fail("Not yet implemented");
+    }
+}
+
+// https://readlearncode.com/code-and-stuff/how-do-i-change-eclipse-to-use-spaces-instead-of-tabs/
