373eb6c52022baa64448d48870e15783f75bccdf
leandrocgsi <leandrocgsi@gmail.com>
2023-02-20 21:28:41 -0300

    Advanced Concepts of JUnit 5

--- S04_AdvancedConceptsofJUnit5/src/test/java/br/com/erudio/math/SimpleMathTest.java
+++ S04_AdvancedConceptsofJUnit5/src/test/java/br/com/erudio/math/SimpleMathTest.java
@@ -2,13 +2,11 @@ package br.com.erudio.math;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.stream.Stream;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 @DisplayName("Test Math Operations in SimpleMath Class")
 class SimpleMathTest {
@@ -24,7 +22,20 @@ class SimpleMathTest {
     //@Test
     @ParameterizedTest
     //@MethodSource("testDivisionInputParameters")
-    @MethodSource()
+    //@MethodSource()
+    /**
+    @CsvSource({
+        "Pelé, Football",
+        "Senna, F1",
+        "Keith Moon, ''"
+    })
+    
+    @CsvSource({
+        "6.2, 2, 3.1",
+        "71, 14, 57",
+        "18.3, 3.1, 14.9"
+    })*/
+    @CsvFileSource(resources = "/testDivision.csv")
     void testDivision(
             double firstNumber,
             double secondNumber,
@@ -38,6 +49,7 @@ class SimpleMathTest {
             () -> firstNumber + "/" + secondNumber + " did not produce " + expectedResult + "!");
     }
 
+    /*
     public static Stream<Arguments> testDivision() {
     //public static Stream<Arguments> testDivisionInputParameters() {
         return Stream.of(
@@ -46,4 +58,5 @@ class SimpleMathTest {
             Arguments.of(18.3, 3.1D, 14,9D)
         );
     }
+    */
 }
