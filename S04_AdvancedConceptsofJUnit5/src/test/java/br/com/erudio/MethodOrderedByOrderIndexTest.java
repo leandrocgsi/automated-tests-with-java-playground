53608964e213d69ebf064e40c3b799eaa8674408
leandrocgsi <leandrocgsi@gmail.com>
2023-02-21 11:50:13 -0300

    Advanced Concepts of JUnit 5

--- S04_AdvancedConceptsofJUnit5/src/test/java/br/com/erudio/MethodOrderedByOrderIndexTest.java
+++ S04_AdvancedConceptsofJUnit5/src/test/java/br/com/erudio/MethodOrderedByOrderIndexTest.java
@@ -1,33 +1,28 @@
 package br.com.erudio;
 
 import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class MethodOrderedByName {
+@TestMethodOrder(MethodOrderer.MethodName.class)
+public class MethodOrderedByOrderIndexTest {
 
     @Test
-    @Order(1)
     void testC() {
         System.out.println("Runnig Test C");
     }
     
     @Test
-    @Order(2)
     void testD() {
         System.out.println("Runnig Test D");
     }
     
     @Test
-    @Order(3)
     void testA() {
         System.out.println("Runnig Test A");
     }
     
     @Test
-    @Order(4)
     void testB() {
         System.out.println("Runnig Test B");
     }
