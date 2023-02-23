24670b54459cf38898f14c91926bacea37ab6f7d
leandrocgsi <leandrocgsi@gmail.com>
2023-02-23 11:02:35 -0300

    TDD - Test Driven Development

---
+++ S05_JUnit5MockitoandTDD/src/test/java/br/com/erudio/PersonServiceTest.java
@@ -0,0 +1,17 @@
+package br.com.erudio;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PersonServiceTest {
+
+    // test[System Under Test]_[Condition or State Change]_[Expected Result]
+    @DisplayName("Display Name")
+    @Test
+    void testABCD_When_XYZ_Should() {
+        // Given / Arrange
+        // When / Act
+        // Then / Assert
+    }
+
+}
