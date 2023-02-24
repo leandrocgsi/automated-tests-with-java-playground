712362d80a3fe7c6746bc1a4f75b578dbcd76108
leandrocgsi <leandrocgsi@gmail.com>
2023-02-24 15:27:16 -0300

    Testing Repositories

---
+++ S09_TestingRepositories/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/repositories/PersonRepository.java
@@ -0,0 +1,31 @@
+package br.com.erudio.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import br.com.erudio.model.Person;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+
+    Optional<Person> findByEmail(String email);
+
+    // define custom query using JPQL with index params
+    @Query("select p from Person p where p.firstName = ?1 and p.lastName = ?2")
+    Person findByJPQL(String firstName, String lastName);
+
+    // define custom query using JPQL with named params
+    @Query("select p from Person p where p.firstName =:firstName and p.lastName =:lastName")
+    Person findByJPQLNamedParams(@Param("firstName") String firstName, @Param("lastName") String lastName);
+
+    // define custom query using Native SQL with index params
+    @Query(value = "select * from persons p where p.first_name =?1 and p.last_name =?2", nativeQuery = true)
+    Person findByNativeSQL(String firstName, String lastName);
+
+    // define custom query using Native SQL with named params
+    @Query(value = "select * from persons p where p.first_name =:firstName and p.last_name =:lastName",
+            nativeQuery = true)
+    Person findByNativeSQLNamed(@Param("firstName") String firstName, @Param("lastName") String lastName);
+}
