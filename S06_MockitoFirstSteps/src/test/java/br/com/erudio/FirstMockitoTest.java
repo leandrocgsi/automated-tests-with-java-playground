12b2d2706db097fa42f6d9a0dae08e3aeda602b5
leandrocgsi <leandrocgsi@gmail.com>
2023-03-23 16:41:13 -0300

    Mockito's First Steps

---
+++ S06_MockitoFirstSteps/src/test/java/br/com/erudio/FirstMockitoTest.java
@@ -0,0 +1,14 @@
+package br.com.erudio;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class FirstMockitoTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
