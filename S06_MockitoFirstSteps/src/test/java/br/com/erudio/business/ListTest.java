1f401d638a84439d3f1137ebc56341bc113321cb
leandrocgsi <leandrocgsi@gmail.com>
2023-03-24 17:33:59 -0300

    Mockito's First Steps

---
+++ S06_MockitoFirstSteps/src/test/java/br/com/erudio/business/ListTest.java
@@ -0,0 +1,49 @@
+package br.com.erudio.business;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.mock;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class ListTest {
+
+    @Test
+    public void mockingListSizeTest() {
+        List list = mock(List.class);
+        Mockito.when(list.size()).thenReturn(10);
+        assertEquals(10, list.size());
+        assertEquals(10, list.size());
+        assertEquals(10, list.size());
+    }
+
+    @Test
+    public void mockingListSize_withMultipleReturnValues() {
+        List list = mock(List.class);
+        Mockito.when(list.size()).thenReturn(10).thenReturn(20);
+        assertEquals(10, list.size()); // First Call
+        assertEquals(20, list.size()); // Second Call
+    }
+
+    @Test
+    public void mockingListGet() {
+        List<String> list = mock(List.class);
+        Mockito.when(list.get(0)).thenReturn("Erudio");
+        assertEquals("Erudio", list.get(0));
+        assertNull(list.get(1));
+    }
+
+    @Test
+    public void mockingListGet_withAny() {
+        List<String> list = mock(List.class);
+        Mockito.when(list.get(Mockito.anyInt())).thenReturn("Erudio");
+        // If you are using argument matchers, all arguments
+        // have to be provided by matchers.
+        assertEquals("Erudio", list.get(0));
+        assertEquals("Erudio", list.get(1));
+    }
+
+}
