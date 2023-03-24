8121298ea50582ca589fe5ec6749eed540140dae
leandrocgsi <leandrocgsi@gmail.com>
2023-03-24 16:50:05 -0300

    Mockito's First Steps

--- S06_MockitoFirstSteps/src/test/java/br/com/erudio/business/CourseBusinessMockTest.java
+++ S06_MockitoFirstSteps/src/test/java/br/com/erudio/business/CourseBusinessMockTest.java
@@ -5,6 +5,9 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import static org.mockito.Mockito.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 import br.com.erudio.service.CourseService;
 
 class CourseBusinessMockTest {
@@ -12,6 +15,22 @@ class CourseBusinessMockTest {
     @Test
     void testRetrieveCoursesRelatedToSpring_usingAMock() {
         CourseService mockService = mock(CourseService.class);
+        
+        List<String> courses = Arrays.asList(
+            "REST API's RESTFul do 0 à Azure com ASP.NET Core 5 e Docker",
+            "Agile Desmistificado com Scrum, XP, Kanban e Trello",
+            "Spotify Engineering Culture Desmistificado",
+            "REST API's RESTFul do 0 à AWS com Spring Boot 3 Java e Docker",
+            "Docker do Zero à Maestria - Contêinerização Desmistificada",
+            "Docker para Amazon AWS Implante Apps Java e .NET com Travis CI",
+            "Microsserviços do 0 com Spring Cloud, Spring Boot e Docker",
+            "Arquitetura de Microsserviços do 0 com ASP.NET, .NET 6 e C#",
+            "REST API's RESTFul do 0 à AWS com Spring Boot 3 Kotlin e Docker",
+            "Kotlin para DEV's Java: Aprenda a Linguagem Padrão do Android",
+            "Microsserviços do 0 com Spring Cloud, Kotlin e Docker");
+        
+        // when(mockService.retrieveCourses("Leandro")).thenReturn(courses);
+        
         CourseBusiness business = new CourseBusiness(mockService);
         var filteredCourses = business
             .retrieveCoursesRelatedToSpring("Leandro");
