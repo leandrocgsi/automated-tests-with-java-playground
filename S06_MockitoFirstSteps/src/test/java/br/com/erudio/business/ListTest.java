b77089590c022feabf2fa5e5aac48528ef77a7cc
leandrocgsi <leandrocgsi@gmail.com>
2023-03-27 10:29:22 -0300

    Mockito's First Steps

--- S06_MockitoFirstSteps/src/test/java/br/com/erudio/business/ListTest.java
+++ S06_MockitoFirstSteps/src/test/java/br/com/erudio/business/ListTest.java
@@ -3,7 +3,7 @@ package br.com.erudio.business;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 import java.util.List;
 
@@ -15,7 +15,7 @@ public class ListTest {
     @Test
     public void mockingListSizeTest() {
         List list = mock(List.class);
-        Mockito.when(list.size()).thenReturn(10);
+        when(list.size()).thenReturn(10);
         assertEquals(10, list.size());
         assertEquals(10, list.size());
         assertEquals(10, list.size());
@@ -24,7 +24,7 @@ public class ListTest {
     @Test
     public void mockingListSize_withMultipleReturnValues() {
         List list = mock(List.class);
-        Mockito.when(list.size()).thenReturn(10).thenReturn(20);
+        when(list.size()).thenReturn(10).thenReturn(20);
         assertEquals(10, list.size()); // First Call
         assertEquals(20, list.size()); // Second Call
     }
@@ -32,7 +32,7 @@ public class ListTest {
     @Test
     public void mockingListGet() {
         List<String> list = mock(List.class);
-        Mockito.when(list.get(0)).thenReturn("Erudio");
+        when(list.get(0)).thenReturn("Erudio");
         assertEquals("Erudio", list.get(0));
         assertNull(list.get(1));
     }
@@ -40,7 +40,7 @@ public class ListTest {
     @Test
     public void mockingListGet_withAny() {
         List<String> list = mock(List.class);
-        Mockito.when(list.get(Mockito.anyInt())).thenReturn("Erudio");
+        when(list.get(Mockito.anyInt())).thenReturn("Erudio");
         // If you are using argument matchers, all arguments
         // have to be provided by matchers.
         assertEquals("Erudio", list.get(0));
@@ -50,7 +50,7 @@ public class ListTest {
     @Test
     public void mockingList_throwAnException() {
         List<String> list = mock(List.class);
-        Mockito.when(list.get(Mockito.anyInt())).thenThrow(new RuntimeException("Foo Bar!!"));
+        when(list.get(Mockito.anyInt())).thenThrow(new RuntimeException("Foo Bar!!"));
         // If you are using argument matchers, all arguments
         // have to be provided by matchers.
         assertThrows(RuntimeException.class, () -> {
