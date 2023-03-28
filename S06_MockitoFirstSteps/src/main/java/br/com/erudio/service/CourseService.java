cf45900a93f15a156bdde1356925aecaedfd8bf7
leandrocgsi <leandrocgsi@gmail.com>
2023-03-28 16:50:35 -0300

    Mockito's First Steps

--- S06_MockitoFirstSteps/src/main/java/br/com/erudio/service/CourseService.java
+++ S06_MockitoFirstSteps/src/main/java/br/com/erudio/service/CourseService.java
@@ -7,5 +7,7 @@ public interface CourseService {
     public List<String> retrieveCourses(String student);
     
     public List<String> doSomething(String student);
+    
+    public void deleteCourse(String course);
 
 }
