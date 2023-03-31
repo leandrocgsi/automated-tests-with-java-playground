a8a91e38aae73921e5c72ab080abe6b3d8495ed9
leandrocgsi <leandrocgsi@gmail.com>
2023-03-31 17:07:23 -0300

    Advanced Concepts of Mockito

--- S07_AdvancedConceptsofMockito/src/test/java/br/com/erudio/mockito/SpyTest.java
+++ S07_AdvancedConceptsofMockito/src/test/java/br/com/erudio/mockito/SpyTest.java
@@ -2,7 +2,6 @@ package br.com.erudio.mockito;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,12 +13,15 @@ class SpyTest {
     @Test
     void test() {
         // List<String> mockArrayList = mock(ArrayList.class);
-        List<String> mockArrayList = spy (ArrayList.class);
+        List<String> mockArrayList = spy(ArrayList.class);
         assertEquals(0, mockArrayList.size());
         
-        when(mockArrayList.size()).thenReturn(5);
+        // when(mockArrayList.size()).thenReturn(5);
         mockArrayList.add("Foo Bar");
-        assertEquals(5, mockArrayList.size());
+        // assertEquals(5, mockArrayList.size());
+        assertEquals(1, mockArrayList.size());
+        mockArrayList.remove("Foo Bar");
+        assertEquals(0, mockArrayList.size());
         
     }
 
