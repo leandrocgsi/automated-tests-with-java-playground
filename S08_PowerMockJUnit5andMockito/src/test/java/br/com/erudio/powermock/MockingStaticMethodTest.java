e5221ca1dbecbc93f32a0e9a7883620afde0f2c3
leandrocgsi <leandrocgsi@gmail.com>
2023-04-04 17:19:19 -0300

    PowerMock, JUnit 5 and Mockito

---
+++ S08_PowerMockJUnit5andMockito/src/test/java/br/com/erudio/powermock/MockingStaticMethodTest.java
@@ -0,0 +1,45 @@
+package br.com.erudio.powermock;
+
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import br.com.erudio.powermock.utils.UtilityClass;
+
+@ExtendWith(MockitoExtension.class)
+public class MockingStaticMethodTest {
+    
+    @Mock
+    Dependency dependency;
+    
+    @InjectMocks
+    SystemUnderTest sut;
+
+    @BeforeEach
+    void setup() {
+        // Given / Arrange
+    }
+    
+    @Test
+    void testMockingStaticMethod() {
+        
+        // Given / Arrange
+        List<Integer> stats =  Arrays.asList(1,2,3);
+        when(dependency.retrieveAllStats()).thenReturn(stats);
+        
+        when(UtilityClass.staticMethod(5)).thenReturn(127);
+            
+        // When / Act
+        sut.methodCallingAStaticMethod();
+        
+        // Then / Assert 
+    }
+}
