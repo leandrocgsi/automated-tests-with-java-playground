c962e3400700237ba49de0f346ee1c548cf7a541
leandrocgsi <leandrocgsi@gmail.com>
2023-04-16 18:20:17 -0300

    Testing Controllers Layer

--- S10_TestingRepositoriesFull/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
+++ S10_TestingRepositoriesFull/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -20,44 +20,44 @@ import br.com.erudio.services.PersonServices;
 @RestController
 @RequestMapping("/person")
 public class PersonController {
-	
-	@Autowired
-	private PersonServices service;
-	
-	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<Person> findAll() {
-		return service.findAll();
-	}
-	
-	@GetMapping(value = "/{id}",
-			produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Person> findById(@PathVariable(value = "id") Long id) {
-		return service.findById(id)
+    
+    @Autowired
+    private PersonServices service;
+    
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Person> findAll() {
+        return service.findAll();
+    }
+    
+    @GetMapping(value = "/{id}",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Person> findById(@PathVariable(value = "id") Long id) {
+        return service.findById(id)
             .map(ResponseEntity::ok)
             .orElseGet(() -> ResponseEntity.notFound().build());
-	}
-	
-	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
-			produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person create(@RequestBody Person person) {
-		return service.create(person);
-	}
-	
-	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
-			produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Person> update(@RequestBody Person person) {
-		try {
-			var updatedPerson = service.update(person);
-			return ResponseEntity.ok(updatedPerson);
-		} catch (Exception e) {
-			return ResponseEntity.notFound().build();
-		}
-	}
-	
-	
-	@DeleteMapping(value = "/{id}")
-	public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
-		service.delete(id);
-		return ResponseEntity.noContent().build();
-	}
+    }
+    
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person create(@RequestBody Person person) {
+        return service.create(person);
+    }
+    
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Person> update(@RequestBody Person person) {
+        try {
+            var updatedPerson = service.update(person);
+            return ResponseEntity.ok(updatedPerson);
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    
+    
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
