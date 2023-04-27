7bc14186de585bb2b28a3406021f9534c84a7891
leandrocgsi <leandrocgsi@gmail.com>
2023-04-27 16:04:47 -0300

    Testing Repositories Layer

---
+++ S10_TestingRepositories/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/erudio/repositories/PersonRepositoryTest.java
@@ -0,0 +1,16 @@
+package br.com.erudio.repositories;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class PersonRepositoryTest {
+
+    @Test
+    void test() {
+        fail("Not yet implemented");
+    }
+
+}
