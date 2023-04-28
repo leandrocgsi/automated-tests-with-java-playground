34d6cee543bc9ef9555eaaf26e3c81acb02ab798
leandrocgsi <leandrocgsi@gmail.com>
2023-04-28 16:11:56 -0300

    Testing Repositories Layer

--- S10_TestingRepositories/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/erudio/repositories/PersonRepositoryTest.java
+++ S10_TestingRepositories/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/erudio/repositories/PersonRepositoryTest.java
@@ -1,16 +1,53 @@
 package br.com.erudio.repositories;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import br.com.erudio.model.Person;
 
 @DataJpaTest
 class PersonRepositoryTest {
 
+    @Autowired
+    private PersonRepository personRepository;
+
+    private Person person;
+
+    @BeforeEach
+    public void setup(){
+        person = new Person(
+            "Leandro",
+            "Costa",
+            "leandro@erudio.com.br",
+            "Uberlândia - Minas Gerais - Brasil",
+            "Male"
+        );
+    }
+    
+    // JUnit test for save person operation
+    //@DisplayName("JUnit test for save person operation")
     @Test
-    void test() {
-        fail("Not yet implemented");
+    public void givenPersonObject_whenSave_thenReturnSavedPerson(){
+
+        //given - precondition or setup
+        Person person = new Person(
+                "Leandro",
+                "Costa",
+                "leandro@erudio.com.br",
+                "Uberlândia - Minas Gerais - Brasil",
+                "Male"
+            );
+        // when - action or the behaviour that we are going test
+        Person savedPerson = personRepository.save(person);
+
+        // then - verify the output
+        assertThat(savedPerson).isNotNull();
+        assertThat(savedPerson.getId()).isGreaterThan(0);
     }
 
 }
