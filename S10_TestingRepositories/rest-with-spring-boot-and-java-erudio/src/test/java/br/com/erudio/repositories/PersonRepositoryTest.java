5b1f683c9a4942dde05fe76e45b6db95518a4ef3
leandrocgsi <leandrocgsi@gmail.com>
2023-05-03 17:14:52 -0300

    Testing Repositories Layer

--- S10_TestingRepositories/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/erudio/repositories/PersonRepositoryTest.java
+++ S10_TestingRepositories/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/erudio/repositories/PersonRepositoryTest.java
@@ -1,8 +1,13 @@
 package br.com.erudio.repositories;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
